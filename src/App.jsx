708e44d163999e893ec5a166b7dfff52873f33e9
takbirgazi <takbirgazibd@gmail.com>
2024-03-20 15:38:15 +0600

    Update Info

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
-import NavBar from './components/NavBar/NavBar'
 import Home from './components/Home/Home'
 
 function App() {
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,16 @@
-import { Link, Navigate } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import "./NavBar.css";
 
 export default function NavBar(){
     return(
         <nav className="">
-            <ul className="flex gap-5 items-center font-bold text-[#4b665b]">
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-                <Link to="/bgoevent">BGO Events</Link>
-                <Link to="/resources">Resources</Link>
-                <Link to="/gallery">Gallery</Link>
-                <Link to="/contact">Contact</Link>
+            <ul className="flex gap-5 items-center font-bold text-[#2e3830]">
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/about">About</NavLink>
+                <NavLink to="/bgoevent">BGO Events</NavLink>
+                <NavLink to="/resources">Resources</NavLink>
+                <NavLink to="/gallery">Gallery</NavLink>
+                <NavLink to="/contact">Contact</NavLink>
                 <button className="px-5 py-2 bg-[#00281f] text-white font-bold">Registration</button>
             </ul>
         </nav>
