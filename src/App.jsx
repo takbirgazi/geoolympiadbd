aa7581196dbbce6cc7691c70bc10a4e24ea29196
takbirgazi <takbirgazibd@gmail.com>
2024-03-20 22:10:11 +0600

    Navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
+import { useNavigation } from "react-router-dom";
 import Home from './components/Home/Home'
 
 function App() {
+  const naviGation = useNavigation();
   return (
     <>
       <div>
         <Home></Home>
         <div className='w-11/12 mx-auto'>
-          <Outlet></Outlet>
+          {naviGation.state === "loading" ? "Loading..." : <Outlet></Outlet>}
         </div>
       </div>
     </>
--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,11 +1,19 @@
 import Home from "../Home/Home";
+import { useNavigate, useRouteError } from "react-router-dom";
 
 export default function ErrorPage(){
+    const naviget = useNavigate()
+    const erronStatus = useRouteError();
+    const backHandler = ()=>{
+        naviget(-1);
+    }
+
     return(
         <div>
             <Home></Home>
-            <div className="w-11/12 mx-auto min-h-screen flex items-center justify-center">
-                <h2 className="font-bold text-2xl">404 Page Not Found</h2>
+            <div className="w-11/12 mx-auto min-h-screen flex items-center justify-center flex-col">
+                <h2 className="font-bold text-2xl">{erronStatus.status} {erronStatus.statusText}</h2>
+                <button onClick={backHandler}>Go Back</button>
             </div>
         </div>
     )
